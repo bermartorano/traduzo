0ca49c00731ecec9a19201950d7ee7bd8e8a1b95
Bernardo Martorano <bernardormartorano@gmail.com>
2023-08-14 14:55:13 -0300

    Iniciando o projeto

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,17 +1,16 @@
-# from .abstract_model import AbstractModel
-# from database.db import db
+from .abstract_model import AbstractModel
+from database.db import db
 
 
-# Req. 1
-class LanguageModel:
-    def __init__(self):
-        raise NotImplementedError
+class LanguageModel(AbstractModel):
+    _collection = db['languages']
 
-    # Req. 2
+    def __init__(self, data):
+        super.__init__(self, data)
+
     def to_dict(self):
         raise NotImplementedError
 
-    # Req. 3
     @classmethod
     def list_dicts(cls):
         raise NotImplementedError
