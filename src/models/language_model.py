43097255cbef19ce62b66ba01207ccbe0ba8e98d
Bernardo Martorano <bernardormartorano@gmail.com>
2023-08-14 18:51:23 -0300

    req01

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,12 +1,12 @@
-from .abstract_model import AbstractModel
-from database.db import db
+from src.models.abstract_model import AbstractModel
+from src.database.db import db
 
 
 class LanguageModel(AbstractModel):
     _collection = db['languages']
 
     def __init__(self, data):
-        super.__init__(self, data)
+        super().__init__(data)
 
     def to_dict(self):
         raise NotImplementedError
